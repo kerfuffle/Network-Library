ada45f87c47dcd87ebff434975793eaa51b930eb
kerfuffle <thepolarofbears@gmail.com>
2017-04-05 09:03:56 -0400

    Setup Workspace
    - Begin client, server, packet

---
+++ Code/NetworkLibrary/src/net/kerfuffle/Utilities/Network/MyCode.java
@@ -0,0 +1,7 @@
+package net.kerfuffle.Utilities.Network;
+
+public abstract class MyCode {
+
+	public abstract void run(Object... obj);
+	
+}
---
+++ Code/NetworkLibrary/src/net/kerfuffle/Utilities/Network/Server.java
@@ -0,0 +1,9 @@
+package net.kerfuffle.Utilities.Network;
+
+public abstract class Server extends Thread{
+	
+
+	
+	public abstract void run();
+
+}
